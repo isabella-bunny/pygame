bfe603c4eaf0740e7bc73a0904711283835b7c9d
Huawei Weng <hweng@ucsd.edu>
2021-12-11 15:20:12 -0800

    bella's first commit.

---
+++ 1st_project.py
@@ -0,0 +1,35 @@
+# Pygame template - skeleton for a new pygame project
+import pygame
+import random
+import settings
+
+
+# initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((settings.WIDTH, HEIGHT))
+pygame.display.set_caption("TITLE")
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+    # Update
+    all_sprites.update()
+
+
+    # Draw / render
+    screen.fill(BLUE)
+    all_sprites.draw(screen)
+    # *after* drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
---
+++ setting.py
@@ -0,0 +1,14 @@
+WIDTH = 360
+HEIGHT = 480
+FPS = 31
+
+
+# define colors
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+CYAN = (0, 255, 255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+PINK = (255, 0, 255)
